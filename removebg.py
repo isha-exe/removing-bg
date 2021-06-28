25d3feaa722d5b2777eff05a8b44f77abc5a572b
Isha Narang <narangisha13@gmail.com>
2021-06-28 17:47:33 +0530

    Update removebg.py

--- removebg.py
+++ removebg.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-img = cv2.imread("assets/smiley.jpg")
+img = cv2.imread("smiley.jpg")
 gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 _, thresh = cv2.threshold(gray_img, 127, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
 img_contours = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[-2]
